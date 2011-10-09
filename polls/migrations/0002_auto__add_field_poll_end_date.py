1893187c3d6e7343fd1446f5a6245598307671a5
Vanessa Hamer <vhamer@gmail.com>
2011-10-09 17:59:52 -0400

    added heavymetal data and added end date to Poll model

---
+++ polls/migrations/0002_auto__add_field_poll_end_date.py
@@ -0,0 +1,38 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Poll.end_date'
+        db.add_column('polls_poll', 'end_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Poll.end_date'
+        db.delete_column('polls_poll', 'end_date')
+
+
+    models = {
+        'polls.choice': {
+            'Meta': {'object_name': 'Choice'},
+            'choice': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'poll': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['polls.Poll']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'polls.poll': {
+            'Meta': {'object_name': 'Poll'},
+            'end_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'question': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['polls']
--- polls/models.py
+++ polls/models.py
@@ -5,10 +5,21 @@ from django.db import models
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True,null=True)
+    def __unicode__(self):
+        return self.question
     
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+    def __unicode__(self):
+        return self.choice
+        
+import datetime 
+
+class Poll(models.Model):
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
     
     
